01853b68ac530e59d787c984bccf7a62556e3cca
VitorMendonca62 <vitorqueiroz325@gmail.com>
2025-01-16 15:18:45 -0300

    (test) teste no usercontroller.create ou POST users/

--- src/modules/user/adaptars/primary/http/user.controller.spec.ts
+++ src/modules/user/adaptars/primary/http/user.controller.spec.ts
@@ -1,18 +1,161 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { CreateUserUseCase } from '@user/core/application/use-cases/create-user.usecase';
+import { DeleteUserUseCase } from '@user/core/application/use-cases/delete-user.usecase';
+import { GetUserUseCase } from '@user/core/application/use-cases/get-user.usecase';
+import { GetUsersUseCase } from '@user/core/application/use-cases/get-users.usecase';
+import { UpdateUserUseCase } from '@user/core/application/use-cases/update-user.usecase';
+import { UserMapper } from '../../mappers/user.mapper';
+import { INestApplication } from '@nestjs/common';
+import { mockCreateDTO, mockUser } from '@user/helpers/tests.helper';
+import * as request from 'supertest';
 
-describe('HttpController', () => {
+describe('UserController', () => {
+  let app: INestApplication;
+
   let controller: UserController;
+
+  let mapper: UserMapper;
+
+  let createUserUseCase: CreateUserUseCase;
+  let getUserUseCase: GetUserUseCase;
+  let getUsersUseCase: GetUsersUseCase;
+  let updateUserUseCase: UpdateUserUseCase;
+  let deleteUserUseCase: DeleteUserUseCase;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        UserMapper,
+        CreateUserUseCase,
+        GetUserUseCase,
+        GetUsersUseCase,
+        UpdateUserUseCase,
+        DeleteUserUseCase,
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+
+    mapper = module.get<UserMapper>(UserMapper);
+
+    createUserUseCase = module.get<CreateUserUseCase>(CreateUserUseCase);
+    getUserUseCase = module.get<GetUserUseCase>(GetUserUseCase);
+    getUsersUseCase = module.get<GetUsersUseCase>(GetUsersUseCase);
+    updateUserUseCase = module.get<UpdateUserUseCase>(UpdateUserUseCase);
+    deleteUserUseCase = module.get<DeleteUserUseCase>(DeleteUserUseCase);
+
+    app = module.createNestApplication();
+    await app.init();
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(mapper).toBeDefined();
+    expect(createUserUseCase).toBeDefined();
+    expect(getUserUseCase).toBeDefined();
+    expect(getUsersUseCase).toBeDefined();
+    expect(updateUserUseCase).toBeDefined();
+    expect(deleteUserUseCase).toBeDefined();
+    expect(app).toBeDefined();
+  });
+
+  describe('POST /', () => {
+    beforeEach(() => {
+      jest.spyOn(mapper, 'create').mockImplementation((dto) => mockUser(dto));
+      jest
+        .spyOn(createUserUseCase, 'execute')
+        .mockImplementation(() => undefined);
+    });
+
+    it('should use case call with correct parameters', async () => {
+      const dto = mockCreateDTO();
+      await controller.create(dto);
+
+      expect(mapper.create).toHaveBeenCalledWith(dto);
+      expect(createUserUseCase.execute).toHaveBeenCalledWith(mockUser());
+    });
+
+    it('should create user and return sucess message', async () => {
+      const dto = mockCreateDTO();
+
+      const response = await request(app.getHttpServer())
+        .post('/user')
+        .send(dto)
+        .expect(201);
+
+      expect(response.body).toEqual({
+        message: 'Usuário criado com sucesso',
+        data: undefined,
+      });
+    });
+
+    it('should throw bad request error when no have fields', async () => {
+      const dto = mockCreateDTO({
+        email: undefined,
+        name: undefined,
+        password: undefined,
+        phonenumber: undefined,
+        username: undefined,
+      });
+
+      const response = await request(app.getHttpServer())
+        .post('/user')
+        .send(dto)
+        .expect(400);
+
+      expect(response.body).toEqual({
+        error: 'Bad Request',
+        message: [
+          'O username é obrigatório',
+          'O nome completo é obrigatório',
+          'O email é obrigatório',
+          'A senha é obrigatória',
+          'O telefone é obrigatório',
+        ],
+        statusCode: 400,
+      });
+    });
+
+    it('should throw bad request error when invalid phonenumber, username and email', async () => {
+      const dto = mockCreateDTO({
+        email: 'testeexemplo.com',
+        phonenumber: '+23',
+        username: 'default username',
+      });
+
+      const response = await request(app.getHttpServer())
+        .post('/user')
+        .send(dto)
+        .expect(400);
+
+      expect(response.body).toEqual({
+        error: 'Bad Request',
+        message: [
+          'O username não pode conter com espaços.',
+          'O email tem que ser válido',
+          'O telefone deve ser válido do Brasil',
+        ],
+        statusCode: 400,
+      });
+    });
+
+    it('should throw bad request error when invalid phonenumber', async () => {
+      const dto = mockCreateDTO({
+        phonenumber: '+55 8199999999',
+      });
+
+      const response = await request(app.getHttpServer())
+        .post('/user')
+        .send(dto)
+        .expect(400);
+
+      expect(response.body).toEqual({
+        error: 'Bad Request',
+        message: ['O telefone deve ser válido do Brasil'],
+        statusCode: 400,
+      });
+    });
   });
 });
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './adaptars/primary/http/user.controller';
+import { UserMapper } from './adaptars/mappers/user.mapper';
+import { CreateUserUseCase } from './core/application/use-cases/create-user.usecase';
+import { DeleteUserUseCase } from './core/application/use-cases/delete-user.usecase';
+import { GetUserUseCase } from './core/application/use-cases/get-user.usecase';
+import { GetUsersUseCase } from './core/application/use-cases/get-users.usecase';
+import { UpdateUserUseCase } from './core/application/use-cases/update-user.usecase';
 
 @Module({
-  controllers: [UserController]
+  controllers: [UserController],
+  providers: [
+    UserMapper,
+    CreateUserUseCase,
+    GetUserUseCase,
+    GetUsersUseCase,
+    UpdateUserUseCase,
+    DeleteUserUseCase,
+  ],
 })
 export class UserModule {}
