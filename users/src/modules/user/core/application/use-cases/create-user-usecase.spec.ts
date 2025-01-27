134176cafb2dae578e03b63e17d34929d29d665a
VitorMendonca62 <vitorqueiroz325@gmail.com>
2025-01-27 12:11:59 -0300

    (chore) removendo regras de negocios desnecessarias em create-users-use-case em ms-users

--- users/src/modules/user/core/application/use-cases/create-user-usecase.spec.ts
+++ users/src/modules/user/core/application/use-cases/create-user-usecase.spec.ts
@@ -4,7 +4,6 @@ import { CreateUserUseCase } from './create-user.usecase';
 import { mockUser } from '@modules/user/helpers/tests.helper';
 import { InMemoryUserRepository } from '@user/adaptars/secondary/database/repositories/inmemory-user.repository';
 import { ConfigModule } from '@nestjs/config';
-import { BadRequestException } from '@nestjs/common';
 
 describe('CreateUserUseCase', () => {
   let useCase: CreateUserUseCase;
@@ -35,47 +34,14 @@ describe('CreateUserUseCase', () => {
     const user = mockUser();
 
     beforeEach(() => {
-      jest
-        .spyOn(userRepository, 'findByUsername')
-        .mockImplementation(async () => undefined);
-      jest
-        .spyOn(userRepository, 'findByEmail')
-        .mockImplementation(async () => undefined);
-
       jest.spyOn(userRepository, 'create').mockImplementation(() => undefined);
     });
 
     it('should use case call with correct parameters and create user', async () => {
       const response = await useCase.execute(user);
 
-      expect(userRepository.findByEmail).toHaveBeenCalledWith(user.email);
-      expect(userRepository.findByUsername).toHaveBeenCalledWith(user.username);
       expect(userRepository.create).toHaveBeenCalledWith(user);
       expect(response).toBeUndefined();
-    });
-
-    it('should throw bad request exception when already exists user with newUser email', async () => {
-      jest
-        .spyOn(userRepository, 'findByEmail')
-        .mockImplementation(async () => user);
-
-      await expect(useCase.execute(user)).rejects.toThrow(
-        new BadRequestException(
-          'Esse email já está sendo utilizado. Tente outro',
-        ),
-      );
-    });
-
-    it('should throw bad request exception when already exists user with newUser username', async () => {
-      jest
-        .spyOn(userRepository, 'findByUsername')
-        .mockImplementation(async () => user);
-
-      await expect(useCase.execute(user)).rejects.toThrow(
-        new BadRequestException(
-          'Esse username já está sendo utilizado. Tente outro',
-        ),
-      );
     });
   });
 });
--- users/src/modules/user/core/application/use-cases/create-user.usecase.ts
+++ users/src/modules/user/core/application/use-cases/create-user.usecase.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { User } from '../../domain/entities/user.entity';
 import { CreateUserPort } from '../ports/primary/user.port';
 import { UserRepository } from '../ports/secondary/user-repository.interface';
@@ -8,26 +8,6 @@ export class CreateUserUseCase implements CreateUserPort {
   constructor(private readonly userRepository: UserRepository) {}
 
   async execute(user: User): Promise<void> {
-    const userExistsWithUsername = await this.userRepository.findByUsername(
-      user.username,
-    );
-
-    if (userExistsWithUsername) {
-      throw new BadRequestException(
-        'Esse username já está sendo utilizado. Tente outro',
-      );
-    }
-
-    const userExistsWithEmail = await this.userRepository.findByEmail(
-      user.email,
-    );
-
-    if (userExistsWithEmail) {
-      throw new BadRequestException(
-        'Esse email já está sendo utilizado. Tente outro',
-      );
-    }
-
     await this.userRepository.create(user);
   }
 }
