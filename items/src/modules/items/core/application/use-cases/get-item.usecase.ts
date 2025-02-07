eddc69a16e1a175e962a926c7cb0d349b5339de8
VitorMendonca62 <vitorqueiroz325@gmail.com>
2025-02-07 10:40:30 -0300

    feat: test on getitemusecase in ms-items

--- items/src/modules/items/core/application/use-cases/get-item.usecase.ts
+++ items/src/modules/items/core/application/use-cases/get-item.usecase.ts
@@ -1,8 +1,19 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Item } from '../../domain/entities/item.entity';
 import { GetItemPort } from '../ports/primary/item.port';
+import { ItemRepository } from '../ports/secondary/item-repository.interface';
 
+@Injectable()
 export class GetItemUseCase implements GetItemPort {
-  findById(id: string): Promise<Item> {
-    throw new Error('Method not implemented.');
+  constructor(private readonly itemRepository: ItemRepository) {}
+
+  async findById(id: string): Promise<Item> {
+    const item = await this.itemRepository.findById(id);
+
+    if (!item) {
+      throw new NotFoundException('Não foi possivel encontrar o item');
+    }
+
+    return item;
   }
 }
