2f358a896ecf1b9b53e2353bb2c46dfe53cb3e15
Vitor Mendonca <vitorqueiroz325@gmail.com>
2025-07-28 21:03:39 -0300

    feat: add environment validation tests for configuration variables

---
+++ auth/src/config/environment/env.validate.spec.ts
@@ -0,0 +1,76 @@
+import { EnvironmentVariables, NodeEnv } from './env.validation';
+
+import * as classTransformer from 'class-transformer';
+import * as classValidator from 'class-validator';
+import { validateENV } from './env.validate';
+
+describe('validateENV', () => {
+  const mockEnv: Record<string, unknown> = {
+    NODE_ENV: 'development',
+    HOST: 'localhost',
+    PORT: 8080,
+    MESSAGING_HOST: '127.0.0.1',
+    MESSAGING_USER: 'default',
+    MESSAGING_PW: '1234',
+    MESSAGING_PORT: 6379,
+    JWT_SECRET: 'jwtsecret123',
+    API_TAG: 'test_auth',
+    MONGO_DB_URL: 'mongodb://localhost:27017/test_auth_users',
+  };
+
+  const mockInstance = new EnvironmentVariables();
+  mockInstance.NODE_ENV = NodeEnv.Development;
+  mockInstance.HOST = 'localhost';
+  mockInstance.PORT = 8080;
+  mockInstance.MESSAGING_HOST = '127.0.0.1';
+  mockInstance.MESSAGING_USER = 'default';
+  mockInstance.MESSAGING_PW = '1234';
+  mockInstance.MESSAGING_PORT = 6379;
+  mockInstance.JWT_SECRET = 'jwtsecret123';
+  mockInstance.API_TAG = 'test_auth';
+  mockInstance.MONGO_DB_URL = 'mongodb://localhost:27017/test_auth_users';
+
+  let plainToInstanceMocked: jest.SpyInstance;
+  let validateSyncMocked: jest.SpyInstance;
+
+  beforeAll(() => {
+    plainToInstanceMocked = jest.spyOn(classTransformer, 'plainToInstance');
+
+    validateSyncMocked = jest.spyOn(classValidator, 'validateSync');
+  });
+
+  beforeEach(() => {
+    plainToInstanceMocked.mockReturnValue(mockInstance);
+    validateSyncMocked.mockReturnValue([]);
+  });
+
+  it('should return validated config when valid', () => {
+    const result = validateENV(mockEnv);
+
+    expect(plainToInstanceMocked).toHaveBeenCalledWith(
+      EnvironmentVariables,
+      mockEnv,
+      {
+        enableImplicitConversion: true,
+      },
+    );
+
+    expect(validateSyncMocked).toHaveBeenCalledWith(mockInstance, {
+      skipMissingProperties: false,
+    });
+
+    expect(result).toEqual(mockInstance);
+  });
+
+  it('should throw error if validation fails', () => {
+    const mockError = {
+      constraints: {
+        isString: 'NODE_ENV must be a string',
+      },
+    };
+
+    validateSyncMocked.mockReturnValue([mockError]);
+
+    expect(() => validateENV(mockEnv)).toThrow('NODE_ENV must be a string');
+  });
+});
