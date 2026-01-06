97408305cebb36edcb438e1c32fd6bc47683a9a2
VitorMendonca62 <vitorqueiroz325@gmail.com>
2026-01-06 10:28:57 -0300

    refact-auth: extract Swagger, CORS, and validation pipe configurations into AppConfig class for better organization

---
+++ auth/src/config/app.config.ts
@@ -0,0 +1,97 @@
+import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables, NodeEnv } from './environment/env.validation';
+import { Cookies } from '@auth/domain/enums/cookies.enum';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { FieldInvalid } from '@auth/domain/ports/primary/http/errors.port';
+
+export default class AppConfig {
+  constructor(
+    private readonly configService: ConfigService<EnvironmentVariables>,
+    private readonly app: INestApplication,
+  ) {}
+
+  configSwagger() {
+    if (this.configService.get('NODE_ENV') === NodeEnv.Production) return;
+
+    const documentBuilder = new DocumentBuilder()
+      .setTitle('Auth System')
+      .setDescription('The auth system for a e-commerce store')
+      .setVersion('1.0')
+      .addCookieAuth(
+        Cookies.RefreshToken,
+        {
+          type: 'apiKey',
+          in: 'cookie',
+          name: Cookies.RefreshToken,
+        },
+        'cookie_refresh',
+      )
+      .addCookieAuth(
+        Cookies.AccessToken,
+        {
+          type: 'apiKey',
+          in: 'cookie',
+          name: Cookies.AccessToken,
+        },
+        'cookie_access',
+      )
+      .addCookieAuth(
+        Cookies.ResetPassToken,
+        {
+          type: 'apiKey',
+          in: 'cookie',
+          name: Cookies.ResetPassToken,
+        },
+        'cookie_reset_pass',
+      )
+      .build();
+
+    const document = SwaggerModule.createDocument(this.app, documentBuilder);
+
+    SwaggerModule.setup('docs', this.app, document, {
+      swaggerOptions: {
+        requestInterceptor: (req) => {
+          req.credentials = 'include';
+          return req;
+        },
+      },
+    });
+  }
+
+  configCors() {
+    // TODO: Configurar os hosts dps
+    // app.enableCors({
+    //   origin:
+    //     this.configService.get('NODE_ENV') === NodeEnv.Production
+    //       ? `http://localhost:`
+    //       : [`http://localhost:${PORT}`],
+    //   credentials: true,
+    //   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    //   allowedHeaders: ['Content-Type', 'Authorization'],
+    // });
+  }
+
+  configValidationPipe() {
+    this.app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        stopAtFirstError: true,
+        transform: false,
+        exceptionFactory: (errors) => {
+          if (errors.length == 0) {
+            return new FieldInvalid('Unknown error', 'Error');
+          }
+
+          const firstError = errors[0];
+          const firstConstraintMessage = firstError.constraints
+            ? Object.values(firstError.constraints)[0]
+            : 'Unknown error';
+
+          return new FieldInvalid(firstConstraintMessage, firstError.property);
+        },
+      }),
+    );
+  }
+}
--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,16 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { Logger, ValidationPipe } from '@nestjs/common';
-import { FieldInvalid } from '@auth/domain/ports/primary/http/errors.port';
-import {
-  EnvironmentVariables,
-  NodeEnv,
-} from './config/environment/env.validation';
+import { Logger } from '@nestjs/common';
+import { EnvironmentVariables } from './config/environment/env.validation';
 import * as cookieParser from 'cookie-parser';
 import * as express from 'express';
-import { Cookies } from '@auth/domain/enums/cookies.enum';
+import AppConfig from '@config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,88 +17,16 @@ async function bootstrap() {
   const PORT = configService.get<number>('PORT') ?? 3333;
   const HOST = configService.get<string>('HOST');
 
-  // TODO: Configurar os hosts dps
-  app.enableCors({
-    origin:
-      configService.get('NODE_ENV') === NodeEnv.Production
-        ? `http://localhost:${PORT}`
-        : [`http://localhost:${PORT}`],
-    credentials: true,
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-
   app.use(cookieParser());
-
-  const config = new DocumentBuilder()
-    .setTitle('Auth System')
-    .setDescription('The auth system for a e-commerce store')
-    .setVersion('1.0')
-    .addCookieAuth(
-      Cookies.RefreshToken,
-      {
-        type: 'apiKey',
-        in: 'cookie',
-        name: Cookies.RefreshToken,
-      },
-      'cookie_refresh',
-    )
-    .addCookieAuth(
-      Cookies.AccessToken,
-      {
-        type: 'apiKey',
-        in: 'cookie',
-        name: Cookies.AccessToken,
-      },
-      'cookie_access',
-    )
-    .addCookieAuth(
-      Cookies.ResetPassToken,
-      {
-        type: 'apiKey',
-        in: 'cookie',
-        name: Cookies.ResetPassToken,
-      },
-      'cookie_reset_pass',
-    )
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-
-  if (configService.get('NODE_ENV') !== NodeEnv.Production) {
-    SwaggerModule.setup('docs', app, document, {
-      swaggerOptions: {
-        requestInterceptor: (req) => {
-          req.credentials = 'include';
-          return req;
-        },
-      },
-    });
-  }
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      stopAtFirstError: true,
-      transform: false,
-      exceptionFactory: (errors) => {
-        if (errors.length == 0) {
-          return new FieldInvalid('Unknown error', 'Error');
-        }
-
-        const firstError = errors[0];
-        const firstConstraintMessage = firstError.constraints
-          ? Object.values(firstError.constraints)[0]
-          : 'Unknown error';
-
-        return new FieldInvalid(firstConstraintMessage, firstError.property);
-      },
-    }),
-  );
 
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+  const appConfig = new AppConfig(configService, app);
+
+  appConfig.configSwagger();
+  appConfig.configValidationPipe();
+  appConfig.configCors();
 
   await app
     .listen(PORT, () => appLogger.debug(`Server running in ${HOST}:${PORT}`))
