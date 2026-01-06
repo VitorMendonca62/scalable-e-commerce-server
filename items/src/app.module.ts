f777f070a7d0f8aa4c95ffaa12bb2ed36854992b
VitorMendonca62 <vitorqueiroz325@gmail.com>
2026-01-06 13:08:18 -0300

    remove: remote items diretory

--- items/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ItemModule } from './modules/item/item.module';
-import { ConfigModule } from '@nestjs/config';
-
-@Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ItemModule],
-})
-export class AppModule {}
--- items/src/modules/items/core/application/mappers/item.mapper.ts
+++
@@ -1,5 +0,0 @@
-import { CreateItemDTO } from '@modules/items/adaptars/primary/http/dtos/create-item.dto';
-
-export class ItemMapper {
-  createItemDTOForEntity(dto: CreateItemDTO): Item {}
-}
