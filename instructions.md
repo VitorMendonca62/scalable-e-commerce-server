fb063365b3d52276db08dacfb775b3bbc3290ff8
VitorMendonca62 <vitorqueiroz325@gmail.com>
2026-01-08 01:56:34 +0000

    chore: authentication configuration

--- instructions.md
+++
@@ -1,9 +0,0 @@
-## Criar rede de mensageria:
-```bash
-docker network create scalable-commerce-net
-```
-
-## Iniciar container para mensageria
-´´´bash
-docker run -d --name scalable-commerce-net --network scalable-commerce-net -p 15672:15672 -p 5672:5672  -e RABBITMQ_DEFAULT_USER=vhmendonca -e RABBITMQ_DEFAULT_PASS=Vh12345678 rabbitmq:3-management-alpine
-´´´
--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
-TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjNlNDU2Ny1lODliLTEyZDMtYTQ1Ni00MjY2MTQxNzQwMDAiLCJqdGkiOiIwMTliOTYyZS1hZjhlLTc2NmItODdmMC0wZGQzNmZjMTIxNjYiLCJ0eXBlIjoicmVmcmVzaCJ9.kiltpzanhF3dNmvUc1vNLmdBImf9SC7ceu9g6O-fMlE"
+TOKEN="eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Imp3dHNlY3JldDEyMyJ9.eyJzdWIiOiIxMjNlNDU2Ny1lODliLTEyZDMtYTQ1Ni00MjY2MTQxNzQwMDAiLCJlbWFpbCI6InZpdG9ybXNpMjAwNUBnbWFpbC5jb20iLCJyb2xlcyI6WyJlbnRlciIsInJlYWRfaXRlbXMiXSwidHlwZSI6ImFjY2VzcyIsImlhdCI6MTc2NzgzNDg3NiwiZXhwIjoxNzY3ODM4NDc2fQ.QibBtK1fZiBXkASf-eI8vgqvTvJUrRsU6NvFw8yG-BkwCnpNSoeWxpOqwncYfZL-eWObjnNhDM7K9cZXYWoGM-EAX3lGk-a0D_X5vE05EzMpW-CCDSvKmZFuEXcpmWWdGx6E4T2Lkj4E0sxXulKepEb99IF0-m95JUkJgjkkPUzKKKq1IKPwPWAWPt417Ch0Mh5usP1CwzUFgXeoKSkhJcg5H3VdU7DOHRQUDRq7Z8mElMZh2PMzEcz_FhvMU9lhr91zn4VoOHDXSO4qSNRLzFFne5C3d6IGehOs8HTfhdYmSBtxRDejVbPyjLalHjB1KUG0nmx1dfMVqLEkWKL0EA"
 
 for i in {1..2}; do 
-curl -i -X POST "https://opinionated-digestibly-jordon.ngrok-free.dev/v1/login" \
+curl -i -X PATCH "http://127.0.0.1:8000/v1/user/password" \
   -b "refresh_token=$TOKEN" \
-  -d '{"email":"vitormsi2005@gmail.com", "password":"@Aa123456781"}' \
+  -b "access_token=$TOKEN" \
   -H "Content-Type: application/json" \
+  -d '{"oldPassword": "@Aa12345678", "newPassword":"@Aa123456781"}' \
   # --cookie "refresh_token=$TOKEN" \
 done
 
