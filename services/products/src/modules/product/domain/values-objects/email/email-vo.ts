2ec3d33b1cd5e5e897520eab0ae20b3806884888
Vitor Mendonça <vitorqueiroz325@gmail.com>
2026-05-14 13:51:38 -0300

    remove-products: removendo codigo desnecessario

--- services/products/src/modules/product/domain/values-objects/email/email-vo.ts
+++
@@ -1,7 +0,0 @@
-import { ValueObject } from '../value-object';
-
-export default class EmailVO extends ValueObject<string | undefined | null> {
-  constructor(value: string | undefined | null) {
-    super(value);
-  }
-}
--- services/products/src/modules/product/domain/values-objects/id/id-vo.ts
+++
@@ -1,7 +0,0 @@
-import { ValueObject } from '../value-object';
-
-export default class IDVO extends ValueObject<string | undefined | null> {
-  constructor(value: string | undefined | null) {
-    super(value);
-  }
-}
